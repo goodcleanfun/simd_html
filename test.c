02cae5fd924c11852afe59250c8d39be28703acb
Al <albarrentine@gmail.com>
2025-08-13 04:55:07 -0400

    [fix] removing debug prints to see if clang issue is something silly

--- test.c
+++ test.c
@@ -13,15 +13,12 @@ TEST test_html_scan(void) {
     const char *html = "<!doctype html><html><head><title>A Document</title><link rel=\"stylesheet\" href=\"style.css\"></head><body><h1>Hello, World!</h1></body></html>";
 
     size_t len = strlen((const char *)html);
-    printf("HTML length: %zu\n", len);
 
     char *data = aligned_malloc(len + 1, 32);
     memcpy(data, html, len);
     data[len] = '\0';
 
     for (simd_html_match_state_t state = simd_html_match_state_init((const char *)data, (const char *)data + len); simd_html_match_state_advance(&state); simd_html_match_state_consume(&state)) {
-        printf("Matches: %zu, end - start = %zu, offset = %zu\n", state.matches, state.end - state.start, state.offset);
-
         ASSERT_EQ(data[state.offset], '<');
     }
 
